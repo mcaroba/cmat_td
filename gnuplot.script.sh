da08be127ec066e0b5f3ff7ccf856d80b4d7914f
Miguel Caro <mcaroba@gmail.com>
2022-11-02 03:23:51 +0000

    Improved format of plot

--- gnuplot.script.sh
+++ gnuplot.script.sh
@@ -10,9 +10,10 @@ set cbrange [0.2:3.8]
 
 unset colorbox
 
-plot "NVTP.dat" u 3:(-(column(4)-8.617e-5*column(1)/column(2)*column(3))/8.617e-5/(column(1)/column(2))**2/column(3) * 1.e-24 * 6.022e23):(column(1)/column(2)*1000.) t "Calculated (MD)" pt 7 lw 4 lc palette, \
-     "optim.dat" u 1:2 w lp t "Experimental" lw 4, \
-     "NVTP.dat" u 3:(-(column(4)-8.617e-5*column(1)/column(2)*column(3))/8.617e-5/(column(1)/column(2))**2/column(3) * 1.e-24 * 6.022e23):5 w labels left not
+plot "NVTP.dat" u 3:(-(column(4)-8.617e-5*column(1)/column(2)*column(3))/8.617e-5/(column(1)/column(2))**2/column(3) * 1.e-24 * 6.022e23):(column(1)/column(2)*1000.) not pt 7 lw 4 lc palette, \
+     "NVTP.dat" u 3:(-(column(4)-8.617e-5*column(1)/column(2)*column(3))/8.617e-5/(column(1)/column(2))**2/column(3) * 1.e-24 * 6.022e23) t "Calculated (MD)" pt 6 lw 2 lc -1, \
+     "optim.dat" u 1:2 w lp t "Experimental" lw 4 lc 2, \
+     "NVTP.dat" u 3:(-(column(4)-8.617e-5*column(1)/column(2)*column(3))/8.617e-5/(column(1)/column(2))**2/column(3) * 1.e-24 * 6.022e23):(sprintf(" %s", stringcolumn(5))) w labels left not
 eof
 
 L=$2
